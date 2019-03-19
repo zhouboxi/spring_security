0a5ebf517b7c0e6092d037e7ea6531a550dd9f48
zhouboxi <zhoubox@topband.com.cn>
2019-03-19 16:57:49 +0800

    启动了修改

--- src/main/java/com/zhouboxi/security/SecurityApplication.java
+++ src/main/java/com/zhouboxi/security/SecurityApplication.java
@@ -2,10 +2,24 @@ package com.zhouboxi.security;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.web.servlet.MultipartAutoConfiguration;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.ServletComponentScan;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+import org.springframework.cache.annotation.EnableCaching;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
-@SpringBootApplication
-public class SecurityApplication {
-
+@SpringBootApplication(exclude = { MultipartAutoConfiguration.class })
+@ServletComponentScan
+@EnableScheduling
+@EnableCaching
+@ComponentScan(basePackages = { "com.zhouboxi.security.tb" })
+public class SecurityApplication extends SpringBootServletInitializer {
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+        return application.sources(SecurityApplication.class);
+    }
     public static void main(String[] args) {
         SpringApplication.run(SecurityApplication.class, args);
     }
